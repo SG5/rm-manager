d155263afbbd9d804fd38fc47131f8ef239b4239
Hello <sergeyv89@gmail.com>
2015-06-25 22:31:30 +0300

    issue list auto-update

--- js/controllers/issue.js
+++ js/controllers/issue.js
@@ -5,12 +5,18 @@ rmManager.controller('issueCtrl', function($scope, $http, config) {
     $scope.issues   = [];
     $scope.statuses = [];
 
-    if (config.userId) {
+    var loadIssueList = function() {
         $http.get(config.url + '/issues.json?assigned_to_id=me')
             .success(function(data) {
                 $scope.issues = data.issues;
             })
         ;
+    };
+
+    if (config.userId) {
+        loadIssueList();
+        setInterval(loadIssueList, 60 * 1000);
+
 
         $http.get(config.url + '/issue_statuses.json')
             .success(function(data) {
